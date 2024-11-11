1eed0b1b757e451c208aa6ad3716bb554f829943
Emil57 <trejocovantesde@gmail.com>
2024-11-10 22:03:29 -0800

    ⚡️Cambios en html egresos

---
+++ Controllers/EgresosController.cs
@@ -0,0 +1,38 @@
+﻿using AgricolaDH_GApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AgricolaDH_GApp.Controllers
+{
+	public class EgresosController : Controller
+	{
+		private readonly ILogger<EgresosController> _logger;
+
+		public EgresosController(ILogger<EgresosController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return PartialView("~/Views/Egresos/Index.cshtml");
+		}
+		[HttpGet]
+        public IActionResult CrearEgreso()
+        {
+            return PartialView("~/Views/Egresos/CrearEgreso.cshtml");
+        }
+
+        public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
