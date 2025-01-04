7988ad679ed88e82e14dbb7699e0488f981a064e
CarlosMaytorena <carlosmaytorena18@outlook.com>
2025-01-03 18:06:21 -0800

    Lectura de XML

---
+++ Models/Factura.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Xml.Serialization;
+
+namespace AgricolaDH_GApp.Models
+{
+    public class Factura
+    {
+        public double Cantidad { get; set; }
+        public double ClaveProdServ {  get; set; }
+        public string CLaveUnidad { get; set; }
+        public string Descripcion { get; set; }
+        public double Importe { get; set; }
+        public int NoIdentificacion { get; set; }
+
+    }
+
+}
