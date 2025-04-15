14f50e6c166bab4e95cd4ae55efe9c4f4bd901ae
Emil57 <trejocovantesde@gmail.com>
2025-04-14 19:00:40 -0700

    Ultimo intento

--- Services/BlobStorageService.cs
+++ Services/BlobStorageService.cs
@@ -23,7 +23,7 @@ public class BlobStorageService
 
             var blobContainerClient = _blobServiceClient.GetBlobContainerClient(_containerName);
             var blobClient = blobContainerClient.GetBlobClient(fileName);
-            using FileStream stream = File.OpenRead(file.FileName);
+            using var stream = file.OpenReadStream();
             await blobClient.UploadAsync(stream, new BlobHttpHeaders { ContentType = file.ContentType });
         }
         catch 
