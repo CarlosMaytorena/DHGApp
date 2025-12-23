d1cbb67517371ed3a86e5bf9a86755351430ee2f
Emil57 <trejocovantesde@gmail.com>
2025-12-23 15:40:58 -0800

    🚨Mejorar manejo de requests en AgregarProductosLista y EliminarProductosLista

--- Controllers/AlmacenController.cs
+++ Controllers/AlmacenController.cs
@@ -95,31 +95,59 @@ namespace AgricolaDH_GApp.Controllers
         [HttpPost]
         public IActionResult AgregarProductoLista(AlmacenVM model, string SourceView)
         {
-            // Verificar si existe en el estado Entrada o Salida segun el caso
-            bool validarEstadoProducto = !almacenService.ValidarEstadoProducto(model.almacen,SourceView); // true si no existe
-
-            bool cond1 = context.Almacen.Any(x => x.SerialNumber.Equals(model.almacen.SerialNumber)); //duplicados Almacen
-            bool cond2 = !model.almacenLista.Exists(x => x.SerialNumber.Equals(model.almacen.SerialNumber)); //duplicados en ListaProductos
-            if (cond1 && cond2 && validarEstadoProducto)
+            try
             {
-                Almacen a = context.Almacen.FirstOrDefault(x => x.SerialNumber.Equals(model.almacen.SerialNumber));
-                a.NombreProducto = context.Productos.FirstOrDefault(x => x.IdProducto.Equals(a.IdProducto)).NombreProducto;
-                a.Descripcion = context.Productos.FirstOrDefault(x => x.IdProducto.Equals(a.IdProducto)).Descripcion;
-                Estatus estatus = context.Estatus.Single(x => x.IdEstatus.Equals(a.IdEstatus));
-                a.Estatus = estatus.NombreEstatus;
+                // Verificar si existe en el estado Entrada o Salida segun el caso
+                bool validarEstadoProducto = !almacenService.ValidarEstadoProducto(model.almacen, SourceView); // true si no existe
 
-                model.almacenLista.Add(a);
+                bool cond1 = context.Almacen.Any(x => x.SerialNumber.Equals(model.almacen.SerialNumber)); //duplicados Almacen
+                bool cond2 = !model.almacenLista.Exists(x => x.SerialNumber.Equals(model.almacen.SerialNumber)); //duplicados en ListaProductos
+                if (cond1 && cond2 && validarEstadoProducto)
+                {
+                    Almacen a = context.Almacen.FirstOrDefault(x => x.SerialNumber.Equals(model.almacen.SerialNumber));
+                    a.NombreProducto = context.Productos.FirstOrDefault(x => x.IdProducto.Equals(a.IdProducto)).NombreProducto;
+                    a.Descripcion = context.Productos.FirstOrDefault(x => x.IdProducto.Equals(a.IdProducto)).Descripcion;
+                    Estatus estatus = context.Estatus.Single(x => x.IdEstatus.Equals(a.IdEstatus));
+                    a.Estatus = estatus.NombreEstatus;
+
+                    model.almacenLista.Add(a);
+                }
+
+                return Ok(new
+                {
+                    message = "Producto agregado correctamente.",
+                    model = model,
+                    html = renderService.RenderViewToStringAsync("~/Views/Almacen/ListaProductos.cshtml", model).Result
+                });
             }
-            return PartialView("~/Views/Almacen/ListaProductos.cshtml", model);
+            catch (Exception ex)
+            {
+                return BadRequest("Error al agregar producto: " + ex.Message);
+            }
         }
+
 
         [HttpPost]
         public IActionResult EliminarProductoLista(AlmacenVM model)
         {
-            if (model.almacenLista.Count > 0)
-                model.almacenLista.RemoveAt(model.almacenLista.Count-1);
-            return PartialView("~/Views/Almacen/ListaProductos.cshtml", model);
+            try
+            {
+                if (model.almacenLista.Count > 0)
+                    model.almacenLista.RemoveAt(model.almacenLista.Count - 1);
+
+                return Ok(new
+                {
+                    message = "Producto eliminado correctamente.",
+                    model = model,
+                    html = renderService.RenderViewToStringAsync("~/Views/Almacen/ListaProductos.cshtml", model).Result
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Error al eliminar producto: " + ex.Message);
+            }
         }
+
 
         public IActionResult GetDetallesProducto(int idProducto)
         {
