098e4f552ee84ed957f9ef07a4de5ec2b645e202
Saul Mendoza <snehemiasms@gmail.com>
2026-01-23 18:21:20 -0800

    reporte-egresos funcionando

--- Models/LogsEgresos.cs
+++ Models/LogsEgresos.cs
@@ -7,11 +7,15 @@ namespace AgricolaDH_GApp.Models
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int IdLogsEgresos { get; set; } //primary key
+        public int IdLogsEgresos { get; set; }
+
         public int IdSolicitante { get; set; }
 
-        [DataType(DataType.Date, ErrorMessage = "Date only")]
+        [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm}", ApplyFormatInEditMode = true)]
         public DateTime Fecha { get; set; }
+
+        public string? Folio { get; set; }
     }
+
 }
